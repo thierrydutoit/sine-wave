5aa25a498a81ed5617d6ea60bea3e535b87da5f2
thierrydutoit <31282663+thierrydutoit@users.noreply.github.com>
2022-05-14 17:35:28 -0400

    Update sinus.py

--- sinus.py
+++ sinus.py
@@ -2,7 +2,7 @@ import streamlit as st
 from numpy import *
 from matplotlib.pyplot import *
 
-st.title('My first sinus wave')
+st.title('My first sine wave')
 a=st.slider('Amplitude a', 0.0, 10.0, 5.0)
 f=st.slider('Frequency f [Hz]', 0, 1000, 440)
 phi=st.slider('Phase phi [rad]', -pi, pi, 0.0)
